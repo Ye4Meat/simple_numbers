ad916cc3abea31b8a80422737c99383912c44339
Ye4Meat <75243932+Ye4Meat@users.noreply.github.com>
2021-02-28 19:04:19 +0500

    Add files via upload

---
+++ ConsoleApplication1.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+
+bool isEqual(int x)
+{
+	return (x == 2 || x == 3 || x == 5 || x == 7);
+}
+
+int main()
+{
+	std::cout << "Enter a number from 1 to 10: ";
+	int x;
+	std::cin >> x;
+
+	bool prime = isEqual(x);
+
+	if (prime)
+		std::cout << "The digit is prime" << std::endl;
+	else
+		std::cout << "The digit is not prime" << std::endl;
+
+	return 0;
+}
